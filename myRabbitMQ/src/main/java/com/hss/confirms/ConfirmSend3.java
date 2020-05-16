d6d5beeeb44c7b4fd810fe34843b77a36ea4da8f
hesensen <hesensen@hztianque.com>
2020-05-16 20:11:29 +0800

    Confirm模式  [异步]

---
+++ myRabbitMQ/src/main/java/com/hss/confirms/ConfirmSend3.java
@@ -0,0 +1,73 @@
+package com.hss.confirms;
+
+import com.hss.util.ConnectionUtils;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConfirmListener;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.MessageProperties;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * confirm
+ * 异步confirm模式：提供一个回调方法
+ */
+public class ConfirmSend3 {
+
+    private static final String QUEUE_NAME="test_queue_confirm";
+
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+        Connection connection = ConnectionUtils.getConnection();
+        Channel channel = connection.createChannel();
+
+        //声明队列
+        boolean durable=true;//消息队列（queue）是否持久化
+        channel.queueDeclare(QUEUE_NAME,durable,false,false,null);
+        //生产者调用confirmSelect 将channel设置为confire模式
+        channel.confirmSelect();
+
+        //未确认的消息标识
+        final SortedSet<Long> confirmSet = Collections.synchronizedSortedSet(new TreeSet<Long>());
+
+        //通道添加监听
+        channel.addConfirmListener(new ConfirmListener() {
+            //没有问题的handleAck
+            @Override
+            public void handleAck(long deliveryTag, boolean multiple) throws IOException {
+                if(multiple){
+                    System.out.println("----------handleAck----------multiple");
+                    confirmSet.headSet(deliveryTag+1).clear();
+                }else{
+                    System.out.println("----------handleAck----------multiple  false");
+                    confirmSet.remove(deliveryTag);
+                }
+            }
+            //handleNack
+            @Override
+            public void handleNack(long deliveryTag, boolean multiple) throws IOException {
+                if(multiple){
+                    System.out.println("----------handleNack----------multiple");
+                    confirmSet.headSet(deliveryTag+1).clear();
+                }else{
+                    System.out.println("----------handleNack----------multiple  false");
+                    confirmSet.remove(deliveryTag);
+                }
+            }
+        });
+
+        for(int i=0;i < 5;i++){
+            String msgString = "hello confirm message["+i+"]";
+            long seqNo = channel.getNextPublishSeqNo();
+            channel.basicPublish("",QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, msgString.getBytes());
+            confirmSet.add(seqNo);
+        }
+
+        channel.close();
+        connection.close();
+
+    }
+}
