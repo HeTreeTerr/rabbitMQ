cb751f89e3e58c111d482140cdb842494c5ec07c
‘HeTreeTerr’ <3110708879@qq.com>
2022-06-18 05:31:36 +0800

    消息确认优化

--- myRabbitMQ/src/main/java/com/hss/confirms/ConfirmSend3.java
+++ myRabbitMQ/src/main/java/com/hss/confirms/ConfirmSend3.java
@@ -1,10 +1,7 @@
 package com.hss.confirms;
 
 import com.hss.util.ConnectionUtils;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.ConfirmListener;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.MessageProperties;
+import com.rabbitmq.client.*;
 
 import java.io.IOException;
 import java.util.Collections;
@@ -59,6 +56,18 @@ public class ConfirmSend3 {
             }
         });
 
+        //接收回退消息的监听器（通常用于路由模式或通配符模式）
+        channel.addReturnListener(new ReturnCallback() {
+            @Override
+            public void handle(Return r) {
+                System.err.println("===========================");
+                System.err.println("Return编码：" + r.getReplyCode() + "-Return描述:" + r.getReplyText());
+                System.err.println("交换机:" + r.getExchange() + "-路由key:" + r.getRoutingKey() );
+                System.err.println("Return主题：" + new String(r.getBody()));
+                System.err.println("===========================");
+            }
+        });
+
         for(int i=0;i < 5;i++){
             String msgString = "hello confirm message["+i+"]";
             long seqNo = channel.getNextPublishSeqNo();
@@ -66,8 +75,9 @@ public class ConfirmSend3 {
             confirmSet.add(seqNo);
         }
 
-        channel.close();
-        connection.close();
+        // ConfirmListener() 是异步的，所以不能关闭连接
+        /*channel.close();
+        connection.close();*/
 
     }
 }
